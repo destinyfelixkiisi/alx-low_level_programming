905769c3336d7f3750a67dfaca2856f664125a31
destinyfelixkiisi <destinyfelixkiisi2006@gmail.com>
2022-03-19 00:34:47 -0700

    Done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main- comments for the main function
+ * Description: Main function also known as the entry point
+ * this program prints "with proper grammar, but the outcome is a piece of art"
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+This is the read me file for the 0x00-hello_world folder
